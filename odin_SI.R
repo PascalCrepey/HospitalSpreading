41f509468caa623e5828c827a2f77f7d8af47e1d
EveRah <eve.rahbe@orange.fr>
2023-02-07 17:26:35 +0100

    odin SI working

--- odin_SI.R
+++ odin_SI.R
@@ -26,9 +26,16 @@ ode_model <- odin::odin(
     #Total population in each hospital i
     N[] <- S[i] + I[i]
 
+    #number of patients moving that are S or I
+    n_S_out[] <- rbinom(sum(d[i,]), S[i]/N[i])
+    n_I_out[] <- sum(d[i,]) - n_S_out[i]
+
+    n_S_in[] <- rbinom(sum(d[,i]), ((sum(S[])-S[i]) / (sum(N[])-N[i])) )
+    n_I_in[] <- sum(d[,i]) - n_S_in[i]
+
     #stochastic update
-    update(S[]) <- S[i] - max(rbinom(S[i], beta*I[i]/N[i]), 0) + sum(d[i,])
-    update(I[]) <- I[i] + max(rbinom(I[i], beta*I[i]/N[i]), 0)
+    update(S[]) <- S[i] - max(rbinom(S[i], beta*I[i]/N[i]), 0) - n_S_out[i] + n_S_in[i]
+    update(I[]) <- I[i] + max(rbinom(I[i], beta*I[i]/N[i]), 0) - n_I_out[i] + n_I_in[i]
 
     #Initial condition
     initial(S[]) <- s_initial
@@ -52,6 +59,12 @@ ode_model <- odin::odin(
     dim(I) <- n_hospitals
 
     dim(d) <- c(n_hospitals, n_hospitals)
+
+    dim(n_S_out) <- n_hospitals
+    dim(n_I_out) <- n_hospitals
+
+    dim(n_S_in) <- n_hospitals
+    dim(n_I_in) <- n_hospitals
 
   }
 )
