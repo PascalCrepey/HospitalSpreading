5f835ede279ce85b323275ba8c3b595f2eb2846e
mlayan <m.layan@hotmail.fr>
2023-02-08 16:09:06 +0100

    Update odin model and add plot

--- R/odin_SI.R
+++ R/odin_SI.R
@@ -4,17 +4,15 @@
 ## INPUT DATA ##
 ################
 
-df_transfer = data.frame(A = c(0, 3, 4),
-                         B = c(5, 0, 1),
-                         C = c(2, 3, 0),
-                         row.names = c("A", "B", "C"))
+df_transfer = matrix(c(0, 5, 2,
+                       3, 0, 3,
+                       4, 1, 0),
+                     ncol = 3, byrow = T)
 
 n_hospitals = 3
 
 s_initial = rep(100, n_hospitals)
-i_initial = rep(1, n_hospitals)
-
-param_beta = 0.16
+i_initial = c(1, rep(0, n_hospitals-1))
 
 ################
 ##    MODEL  ##
@@ -39,20 +37,21 @@ stoch_model_si <- odin::odin(
     t_I_in[] <- sum(t_I[,i])
 
     #stochastic update
-    update(S[]) <- S[i] - max(rbinom(S[i], 1-exp(-beta*I[i]/N[i])), 0) - t_S_out[i] + t_S_in[i]
-    update(I[]) <- I[i] + max(rbinom(I[i], 1-exp(-beta*I[i]/N[i])), 0) - t_I_out[i] + t_I_in[i]
+    new_I[] <- rbinom(S[i], 1-exp(-beta*I[i]/N[i]))
+    update(S[]) <- S[i] - new_I[i] - t_S_out[i] + t_S_in[i]
+    update(I[]) <- I[i] + new_I[i] - t_I_out[i] + t_I_in[i]
 
     #Initial condition
-    initial(S[]) <- s_initial
-    initial(I[]) <- i_initial
+    initial(S[]) <- s_initial[i]
+    initial(I[]) <- i_initial[i]
 
-    #initialize parameters
+    #parameters
     beta <- user()
 
     #initialize user input
     n_hospitals <- user()
-    s_initial <- user()
-    i_initial <- user()
+    s_initial[] <- user()
+    i_initial[] <- user()
     d[,] <- user()
 
     #dimension of parameters
@@ -62,6 +61,9 @@ stoch_model_si <- odin::odin(
     dim(N) <- n_hospitals
     dim(S) <- n_hospitals
     dim(I) <- n_hospitals
+    dim(new_I) <- n_hospitals
+    dim(s_initial) <- n_hospitals
+    dim(i_initial) <- n_hospitals
 
     dim(d) <- c(n_hospitals, n_hospitals)
     dim(t_S) <- c(n_hospitals, n_hospitals)
@@ -73,18 +75,28 @@ stoch_model_si <- odin::odin(
     dim(t_S_in) <- n_hospitals
     dim(t_I_in) <- n_hospitals
 
+    # print variables at each time step
+    output(t_S_out[]) <- TRUE
+    output(t_I_out[]) <- TRUE
+
+    output(t_S_in[]) <- TRUE
+    output(t_I_in[]) <- TRUE
+
   }
 )
 
 #run the model
-model <- stoch_model_si$new(user = pars)
+model <- stoch_model_si$new(beta = 0.16, s_initial = s_initial, i_initial = i_initial,
+                            n_hospitals = 3, d = df_transfer)
 date_lim = 30
-res <- model$run(0:date_lim)
+res <- model$run(0:date_lim, replicate = 1)
 
 ################
 ##    PLOT  ##
 ###############
-
+plot(res[,,1][,1], res[,,1][,2], type = "l", xlab = "Time", ylab = "Number of individuals", col = "black")
+lines(res[,,1][,1], res[,,1][,3], col = "red")
+legend("topright", lwd = 1, col = c("black", "red"), legend = c("S", "I"), bty = "n")
 
 ##################
 ## CALCUL TIME ##
---
+++ tests/testthat/test-odin_SI.R
@@ -0,0 +1,5 @@
+test_that("constant subpopulation size", {
+  res
+
+  expect_equal(out, n_hospitals)
+})
