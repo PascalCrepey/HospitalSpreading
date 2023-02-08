f4721015410cb5d0d7577f3d23a95e66da99beaf
francescoParino <francesco.parino@me.com>
2023-02-08 16:48:25 +0100

    getSimInfModel new func par

--- getSimInfModel.R
+++ getSimInfModel.R
@@ -4,7 +4,7 @@ library(magrittr)
 library(ggplot2)
 library(microbenchmark)
 
-getSimInfModel <- function() {
+getSimInfModel <- function(tspan, npop, n_initial_infected, starting_pop, trans_mat, beta) {
   ##################
   ## Compartments ##
   ##################
@@ -22,21 +22,15 @@ getSimInfModel <- function() {
   ########################
   ## General parameters ##
   ########################
-  gdata = c(beta = 0.16, gamma = 0.077, mu0 = 0, mu1 = 0)
-  tspan_max = 30
-  tspan = 1:tspan_max
+  gdata = c(beta = beta, mu0 = 0, mu1 = 0)
 
   ########################
   ## Add sub population ##
   ########################
-  npop <- 300
 
-  starting_pop = rep(100, npop)
-  index_pop = c(1, rep(0, npop - 1))
 
-  u0 = data.frame(S = starting_pop - index_pop
-                  , I = index_pop)
-
+  u0 = data.frame(S = starting_pop - n_initial_infected
+                  , I = n_initial_infected)
 
   E <- structure(.Data = c(1, 1),
                  .Dim = c(length(compartments),1),
@@ -51,21 +45,9 @@ getSimInfModel <- function() {
               dimnames = list(compartments,
                               c("1", "2")))
 
-  A= diag(npop)
-  reorderIndex = sample(nrow(A))
-  if(any(reorderIndex == 1:npop)){
-    print("running")
-    reorderIndex = sample(nrow(A))
-
-  }
-  trans_mat = A[reorderIndex, ]
-
 
   events <- make_siminf_events(times = tspan, nmetapop = npop, transfer_matrix = trans_mat
                                , select = 1, shift = 0)
-
-  events$event
-
 
   model <- mparse(
     transitions = transitions,
